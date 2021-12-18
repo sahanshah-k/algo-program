4cc5b82b019c1b4434f8eb835e0cd8dc8e20e58b
Sahanshah <k.sahanshah@gmail.com>
2021-12-18 12:42:08 +0530

    Update NextPermutation.java

--- stack-sheet/NextPermutation.java
+++ stack-sheet/NextPermutation.java
@@ -1,5 +1,16 @@
 class Solution {
     
+    /*
+    Step 1 : traverse from right find first index, ie i where a[i] < a[i + 1]
+    Step 2 : traverse from right find the second index j where a[j] > a[i]
+    Step 3 : swap i and j
+    Step 4 : reverse from i + 1 index to length - 1 
+    
+    Step 4 should be done in all the cases
+    */
+    
+    
+    
     public void nextPermutation(int[] A) {
         if (A == null || A.length <= 1) return;
         int i = A.length - 2;
