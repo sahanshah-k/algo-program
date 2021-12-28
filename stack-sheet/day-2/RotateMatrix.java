caa67f8484967d6edd48efeb7074fbc8543d5f67
Sahanshah <k.sahanshah@gmail.com>
2021-12-28 15:12:10 +0530

    Create RotateMatrix.java

---
+++ stack-sheet/day-2/RotateMatrix.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/rotate-image/submissions/
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        //transpose, row to column
+        for (int i = 0; i < n; i ++) {
+            for (int j = i; j < n ; j ++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //reverse each row
+        for (int i = 0; i < n; i ++) {
+            for (int j = 0; j < n/2; j ++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
+            }
+        }
+    }
+}
