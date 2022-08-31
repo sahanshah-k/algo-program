4612e70e88f6cb572163cb9ec6ca2ef5f04d5e0b
Sahanshah <k.sahanshah@gmail.com>
2022-08-31 21:43:09 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (50.66%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+        int a = 1;
+        int b = 2;
+        int c = 0;
+        for (int i = 2; i < n; i ++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
