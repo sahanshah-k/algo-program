43660f52eb8b297c2195e82ca9702486d8cb41b5
Sahanshah <k.sahanshah@gmail.com>
2022-09-04 18:17:17 +0530

    Time: 2 ms (57.40%), Space: 42.1 MB (15.80%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,14 +1,14 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
         
-        HashMap<Integer, Integer> dp = new HashMap<>();
-        dp.put(0, 1);
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
         for (int i = 1; i <= target; i ++) {
-            dp.put(i, 0);
+            dp[i] = 0;
             for (int n: nums) {
-                dp.put(i, dp.get(i) + dp.getOrDefault(i - n, 0));
+                dp[i] += (i - n) >= 0 && (i - n) <= target ? dp[i - n] : 0;
             }
         }
-        return dp.get(target);
+        return dp[target];
     }
 }
