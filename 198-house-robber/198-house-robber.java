4cf009ad93226eed062e6a10aa68e38a31215dba
Sahanshah <k.sahanshah@gmail.com>
2022-09-05 15:28:06 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (46.95%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -3,25 +3,40 @@ class Solution {
         int n = nums.length;
         int[] dp = new int[nums.length];
         Arrays.fill(dp, -1);
-        return solve(nums, n - 1, dp);
+        return solveTab(nums);
     }
     
-    private int solve(int[] nums, int n, int[] dp) {
-        if (n < 0) {
-            return 0;
-        } 
-        if (n == 0) {
-            return nums[0];
-        }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        int incl = solve(nums, n - 2, dp) + nums[n];        
-        int excl = solve(nums, n - 1, dp) + 0;
+//     private int solveMem(int[] nums, int n, int[] dp) {
+//         if (n < 0) {
+//             return 0;
+//         } 
+//         if (n == 0) {
+//             return nums[0];
+//         }
+//         if (dp[n] != -1) {
+//             return dp[n];
+//         }
+//         int incl = solve(nums, n - 2, dp) + nums[n];        
+//         int excl = solve(nums, n - 1, dp) + 0;
         
-        dp[n] = Math.max(incl, excl);
+//         dp[n] = Math.max(incl, excl);
         
-        return dp[n];
+//         return dp[n];
+//     }
+    
+    private int solveTab(int[] nums) {
+        
+        int[] dp = new int[nums.length];
+        
+        dp[0] = nums[0];
+        
+        for (int i = 1; i < nums.length; i ++) {
+            int incl = i >= 2 ? dp[i - 2] + nums[i] : nums[i];
+            int excl = dp[i - 1] + 0;
+            dp[i] = Math.max(incl, excl);
+        }
+        
+        return dp[nums.length - 1];
         
     }
 }
