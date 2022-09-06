00c065913576b088d47e7070daa95011809c7d0a
Sahanshah <k.sahanshah@gmail.com>
2022-09-06 18:57:49 +0530

    Time: 2 ms (67.64%), Space: 42.5 MB (38.83%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.java
@@ -0,0 +1,36 @@
+class Solution {
+    
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        Arrays.fill(dp, -1);
+        return helper(s, 0, dp);
+    }
+    
+    private int helper(String s, int pos, int[] dp) {
+        
+        if (dp[pos] != -1) {
+            return dp[pos];
+        }
+        
+        if (pos == s.length()) {
+            return 1;
+        } 
+        
+        if (s.charAt(pos) == '0') {
+            return 0;
+        }
+                
+        int doubleDigit = 0;
+        int singleDigit = 0;
+                
+        if (pos + 2 <= s.length() && Integer.parseInt(s.substring(pos, pos + 2)) <= 26) {
+            doubleDigit = helper(s, pos + 2, dp);
+            singleDigit = helper(s, pos + 1, dp);
+            dp[pos] = doubleDigit + singleDigit;
+        } else {
+            dp[pos] = helper(s, pos + 1, dp);
+        }            
+        return dp[pos];
+    }
+    
+}
