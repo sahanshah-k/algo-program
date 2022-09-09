0f5080b6f63247136efb3be21364cd20f361f673
Sahanshah <k.sahanshah@gmail.com>
2022-09-09 15:53:35 +0530

    Time: 2 ms (70.18%), Space: 53.8 MB (69.80%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,7 +1,6 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         boolean[] visited = new boolean[isConnected.length];
-        Arrays.fill(visited, false);
         
         int count = 0;
         for (int i = 0; i < isConnected.length; i ++) {
