8892e2bf41674d506984831348601fcfae9a370e
Sahanshah <k.sahanshah@gmail.com>
2022-09-19 15:28:41 +0530

    Time: 3 ms (97.48%), Space: 57.8 MB (26.73%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -3,31 +3,29 @@ class Solution {
         
         int r = grid.length;
         int c = grid[0].length;
-        
-        boolean[][] visited = new boolean[r][c];
-        
+                
         int count = 0;
         
         for (int i = 0; i < r; i ++) {
             for (int j = 0; j < c; j ++) {
-                if (grid[i][j] == '1' && !visited[i][j]) {
+                if (grid[i][j] == '1') {
                     count ++;
-                    dfs(grid, i, j, visited, r, c);
+                    dfs(grid, i, j, r, c);
                 }
             }
         }
         return count;
     }
     
-    void dfs(char[][] grid, int i, int j, boolean[][] visited, int r, int c) {
-        if (isOutside(i, j, r, c) || grid[i][j] == '0' || visited[i][j]) {
+    void dfs(char[][] grid, int i, int j, int r, int c) {
+        if (isOutside(i, j, r, c) || grid[i][j] == '0') {
             return;
         }  
-        visited[i][j] = true;
-        dfs(grid, i + 1, j, visited, r, c);
-        dfs(grid, i, j + 1, visited, r, c);
-        dfs(grid, i - 1, j, visited, r, c);
-        dfs(grid, i, j - 1, visited, r, c);
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j, r, c);
+        dfs(grid, i, j + 1, r, c);
+        dfs(grid, i - 1, j, r, c);
+        dfs(grid, i, j - 1, r, c);
     }
     
     boolean isOutside(int i, int j, int r, int c) {
