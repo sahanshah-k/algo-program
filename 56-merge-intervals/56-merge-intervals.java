a9c0b360295344b79c30c9619b1fe765561a1309
Sahanshah <k.sahanshah@gmail.com>
2022-09-22 18:11:52 +0530

    Time: 9 ms (87.88%), Space: 46.9 MB (98.15%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,31 +1,56 @@
 class Solution {
+//     public int[][] merge(int[][] intervals) {
+        
+//         if (intervals.length <= 1) {
+//             return intervals;
+//         }
+        
+//         Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+//         List<int[]> out = new ArrayList<>();
+        
+//         int[] currentInterval = intervals[0];
+//         out.add(currentInterval);
+
+//         for (int i = 1; i < intervals.length; i ++) {
+//             int currentBegin = currentInterval[0];
+//             int currentEnd = currentInterval[1];
+//             int nextBegin = intervals[i][0];
+//             int nextEnd = intervals[i][1];
+            
+//             if (currentEnd >= nextBegin) {
+//                 currentInterval[1] = Math.max(currentEnd, nextEnd);
+//             } else {
+//                 currentInterval = intervals[i];
+//                 out.add(currentInterval);
+//             }
+            
+//         }
+//         return out.toArray(new int[out.size()][]);
+//     }
+    
     public int[][] merge(int[][] intervals) {
         
-        if (intervals.length <= 1) {
+        if (intervals == null || intervals.length <= 1) {
             return intervals;
         }
         
         Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
         List<int[]> out = new ArrayList<>();
         
-        int[] currentInterval = intervals[0];
-        out.add(currentInterval);
-
+        int[] current = intervals[0];
+        
         for (int i = 1; i < intervals.length; i ++) {
-            int currentBegin = currentInterval[0];
-            int currentEnd = currentInterval[1];
-            int nextBegin = intervals[i][0];
-            int nextEnd = intervals[i][1];
             
-            if (currentEnd >= nextBegin) {
-                currentInterval[1] = Math.max(currentEnd, nextEnd);
+            if (current[1] >= intervals[i][0]) {
+                current[1] = Math.max(current[1], intervals[i][1]);
             } else {
-                currentInterval = intervals[i];
-                out.add(currentInterval);
+                out.add(current);
+                current = intervals[i];
             }
-            
         }
-        return out.toArray(new int[out.size()][]);
+        out.add(current);
+        return out.toArray(new int[out.size()][]); 
     }
+
     
 }
