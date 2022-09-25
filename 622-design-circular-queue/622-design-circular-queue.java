27067918cca40abb5e95c148cfa3f1d3d901fbaa
Sahanshah <k.sahanshah@gmail.com>
2022-09-25 10:07:57 +0530

    Time: 11 ms (15.69%), Space: 47.7 MB (76.80%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.java
+++ 622-design-circular-queue/622-design-circular-queue.java
@@ -1,68 +1,97 @@
 class MyCircularQueue {
 
-    List<Integer> queue;
-    int k;
-    int front;
-    int rear;
+//     List<Integer> queue;
+//     int k;
+//     int front;
+//     int rear;
     
-    public MyCircularQueue(int k) {
-        this.queue = new ArrayList<>();
-        this.k = k;
-        this.front = 0;
-        this.rear = 0;
-        this.queue.add(-1);
-    }
+//     public MyCircularQueue(int k) {
+//         this.queue = new ArrayList<>();
+//         this.k = k;
+//         this.front = 0;
+//         this.rear = 0;
+//         this.queue.add(-1);
+//     }
     
-    public boolean enQueue(int value) {
+//     public boolean enQueue(int value) {
 
-        if (front == rear && queue.get(front) == -1) {
-            queue.set(rear, value);
-            return true;
-        }
+//         if (front == rear && queue.get(front) == -1) {
+//             queue.set(rear, value);
+//             return true;
+//         }
         
-        if (rear - front == k - 1) {
-            return false;
-        }
-        rear ++;
-        //queue.set(rear, value);
-        queue.add(value);
-        return true;
-    }
+//         if (rear - front == k - 1) {
+//             return false;
+//         }
+//         rear ++;
+//         //queue.set(rear, value);
+//         queue.add(value);
+//         return true;
+//     }
     
-    public boolean deQueue() {
-        if (front == rear && queue.get(front) == -1) {
-            return false;
-        }
-        if (front == rear && queue.get(front) != -1) {
-            queue.set(front, -1);
-            return true;
-        }
-        queue.set(front, -1);
-        front ++;
-        return true;
-    }
+//     public boolean deQueue() {
+//         if (front == rear && queue.get(front) == -1) {
+//             return false;
+//         }
+//         if (front == rear && queue.get(front) != -1) {
+//             queue.set(front, -1);
+//             return true;
+//         }
+//         queue.set(front, -1);
+//         front ++;
+//         return true;
+//     }
     
-    public int Front() {
-        if (front == rear && queue.get(front) == -1) {
-            return -1;
+//     public int Front() {
+//         if (front == rear && queue.get(front) == -1) {
+//             return -1;
+//         }
+//         return queue.get(front);
+//     }
+    
+//     public int Rear() {
+//         if (front == rear && queue.get(front) == -1) {
+//             return -1;
+//         }
+//         return queue.get(rear);
+//     }
+    
+//     public boolean isEmpty() {
+//         return front == rear && queue.get(front) == -1;
+//     }
+    
+//     public boolean isFull() {
+//         return rear - front == k - 1;
+//     }
+            final int[] a;
+        int front = 0, rear = -1, len = 0;
+
+        public MyCircularQueue(int k) { a = new int[k];}
+
+        public boolean enQueue(int val) {
+            if (!isFull()) {
+                rear = (rear + 1) % a.length;
+                a[rear] = val;
+                len++;
+                return true;
+            } else return false;
         }
-        return queue.get(front);
-    }
-    
-    public int Rear() {
-        if (front == rear && queue.get(front) == -1) {
-            return -1;
+
+        public boolean deQueue() {
+            if (!isEmpty()) {
+                front = (front + 1) % a.length;
+                len--;
+                return true;
+            } else return false;
         }
-        return queue.get(rear);
-    }
-    
-    public boolean isEmpty() {
-        return front == rear && queue.get(front) == -1;
-    }
-    
-    public boolean isFull() {
-        return rear - front == k - 1;
-    }
+
+        public int Front() { return isEmpty() ? -1 : a[front];}
+
+        public int Rear() {return isEmpty() ? -1 : a[rear];}
+
+        public boolean isEmpty() { return len == 0;}
+
+        public boolean isFull() { return len == a.length;}
 }
 
 /**
