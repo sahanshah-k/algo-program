b64a74f7d0d808ec52004ea38479131a785e00a6
Sahanshah <k.sahanshah@gmail.com>
2022-09-26 23:03:32 +0530

    Time: 2 ms (69.35%), Space: 42.2 MB (73.50%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean equationsPossible(String[] equations) {
+        
+        int[] parent = new int[26];
+        
+        for (int i = 0; i < 26; i ++) {
+            parent[i] = i;
+        }
+        
+        for (String eq: equations) {
+            if (eq.charAt(1) == '=') {
+                int v1 = findParent(eq.charAt(0) - 'a', parent);
+                int v2 = findParent(eq.charAt(3) - 'a', parent);
+                
+                if (v1 != v2) {
+                    parent[v2] = v1; 
+                }
+            }
+        }
+        
+        for (String eq: equations) {
+            if (eq.charAt(1) == '!') {
+                int v1 = findParent(eq.charAt(0) - 'a', parent);
+                int v2 = findParent(eq.charAt(3) - 'a', parent);
+                
+                if (v1 == v2) {
+                    return false;
+                }                
+            }
+        }
+        
+        return true;
+    }
+    
+    private int findParent(int index, int[] parent) {
+        if (parent[index] == index) {
+            return index;
+        }
+        return findParent(parent[index], parent);
+    }
+}
