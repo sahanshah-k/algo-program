2e672cd8ae0363a7a22ffa4c6c077bb22a613c7c
Sahanshah <k.sahanshah@gmail.com>
2022-09-28 11:06:27 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (96.23%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -9,30 +9,50 @@
  * }
  */
 class Solution {
+    // public ListNode removeNthFromEnd(ListNode head, int n) {
+    //     if (n == 1 && head.next == null) {
+    //         return null;
+    //     }
+    //     ListNode temp = head;
+    //     int l = 0;
+    //     while (temp != null) {
+    //         l ++;
+    //         temp = temp.next;
+    //     }
+    //     temp = head;
+    //     n = l - n;
+    //     l = 0;
+    //     if (n == 0) {
+    //         return head.next;
+    //     }
+    //     while (temp != null) {
+    //         l ++;
+    //         if (l == n) {
+    //             temp.next = temp.next.next;
+    //             break;
+    //         }
+    //         temp = temp.next;
+    //     }
+    //     return head;
+    // }
+    
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (n == 1 && head.next == null) {
-            return null;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        
+        for (int i = 0; i <= n; i ++) {
+            first = first.next;
         }
-        ListNode temp = head;
-        int l = 0;
-        while (temp != null) {
-            l ++;
-            temp = temp.next;
+        
+        while (first != null) {
+            first = first.next;
+            second = second.next;
         }
-        temp = head;
-        n = l - n;
-        l = 0;
-        if (n == 0) {
-            return head.next;
-        }
-        while (temp != null) {
-            l ++;
-            if (l == n) {
-                temp.next = temp.next.next;
-                break;
-            }
-            temp = temp.next;
-        }
-        return head;
+        
+        second.next = second.next.next;
+        return dummy.next;
     }
+    
 }
