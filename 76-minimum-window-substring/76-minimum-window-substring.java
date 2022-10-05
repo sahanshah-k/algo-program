e879c362ddba8951ed0fb9ea090e07a5ccd4073e
Sahanshah <k.sahanshah@gmail.com>
2022-10-05 10:26:40 +0530

    Time: 24 ms (54.01%), Space: 47.1 MB (42.67%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -0,0 +1,87 @@
+class Solution {
+//     public String minWindow1(String s, String t) {
+//         if (t.length() == 0 || s.length() == 0) {
+//             return "";
+//         }
+//         int p1 = 0, p2 = 0, min = Integer.MAX_VALUE;
+//         int[] mem = new int[60];
+//         int[] in = new int[60];
+        
+//         formAlpha(t, in);
+        
+//         String out = "";
+        
+//         while (p2 < s.length()) {
+//             while (!check(mem, in) && p2 < s.length()) {
+//                 mem = new int[60];
+//                 formAlphaSol(s, mem, p1, p2 + 1);
+//                 p2 ++;
+//             }
+            
+//             while(check(mem, in)) {
+//                // System.out.println(currentWindow);
+//                 if (p2 - p1 < min && p2 - p1 > 0) {
+//                     out = s.substring(p1, p2);
+//                     min = p2 -p1;
+//                 }   
+//                 //mem = new int[60];
+//                 //formAlphaSol(currentWindow, mem, in);
+//                 mem[s.charAt(p1) - 'A'] = mem[s.charAt(p1) - 'A'] - 1;
+// //                 System.out.println(Arrays.toString(mem));
+// //                 System.out.println(Arrays.toString(in));
+                
+//                 p1 ++;
+//             }
+//         }
+    
+//         return out;
+//     }
+    
+    public String minWindow(String s, String t) {
+        if (s.length() == 0 || t.length() == 0) {
+            return "";
+        }
+        
+        HashMap<Character, Integer> inp = new HashMap<>();
+        HashMap<Character, Integer> mem = new HashMap<>();
+        
+        for (int i = 0; i < t.length(); i ++) {
+            inp.put(t.charAt(i), inp.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        
+        int reqLength = inp.size();
+        int windowLength = 0;
+        int p1 = 0, p2 = 0;
+        int[] out = new int[] {Integer.MAX_VALUE, 0, 0};
+        
+        while (p2 < s.length()) {
+            Character p2Char = s.charAt(p2);
+            if (inp.containsKey(p2Char)) {
+                int newCount = mem.getOrDefault(p2Char, 0) + 1;
+                mem.put(p2Char, newCount);
+                if (newCount == inp.get(p2Char)) {
+                    windowLength ++;
+                }
+            }
+            
+            while (p1 <= p2 && windowLength == reqLength) {
+                if ((p2 - p1 + 1) < out[0]) {
+                    out[0] = p2 - p1 + 1;
+                    out[1] = p1;
+                    out[2] = p2;
+                }
+                Character p1Char = s.charAt(p1);
+                int newCount = mem.getOrDefault(p1Char, 1) - 1;
+                mem.put(p1Char, newCount);
+                if (inp.containsKey(p1Char) && newCount < inp.get(p1Char)) {
+                    windowLength --;
+                }
+                p1 ++;
+            }
+            
+            p2 ++;
+        }
+        return out[0] != Integer.MAX_VALUE ? s.substring(out[1], out[2] + 1) : "";
+    }
+    
+}
