07987a855771abde9615c0bbb3846a23b50f4a1f
Sahanshah <k.sahanshah@gmail.com>
2022-10-10 12:59:46 +0530

    Time: 5 ms (81.25%), Space: 51.7 MB (38.38%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -14,22 +14,49 @@
  * }
  */
 class Solution {
+//     public boolean findTarget(TreeNode root, int k) {
+//         HashSet<Integer> set = new HashSet<>();
+//         return helper(root, set, k);
+//     }
+    
+//     private boolean helper(TreeNode root, HashSet<Integer> set, int k) {
+//         if (root == null) {
+//             return false;
+//         }
+        
+//         boolean left = helper(root.left, set, k);
+//         if (set.contains(root.val)) {
+//             return true;
+//         }
+//         set.add(k - root.val);
+//         boolean right = helper(root.right, set, k);
+//         return right || left;
+//     }
     public boolean findTarget(TreeNode root, int k) {
-        HashSet<Integer> set = new HashSet<>();
-        return helper(root, set, k);
+        List<Integer> list = new ArrayList<>();
+        helper(root, list);
+        int l = 0;
+        int r = list.size() - 1;
+        while (l < r) {
+            int sum = list.get(l) + list.get(r);
+            if (sum == k) {
+                return true;
+            } else if (sum < k) {
+                l ++;
+            } else if (sum > k) {
+                r --;
+            }
+        }
+        return false;
     }
     
-    private boolean helper(TreeNode root, HashSet<Integer> set, int k) {
+    private void helper(TreeNode root, List<Integer> list) {
         if (root == null) {
-            return false;
+            return;
         }
         
-        boolean left = helper(root.left, set, k);
-        if (set.contains(root.val)) {
-            return true;
-        }
-        set.add(k - root.val);
-        boolean right = helper(root.right, set, k);
-        return right || left;
-    }
+        helper(root.left, list);
+        list.add(root.val);
+        helper(root.right, list);
+    }    
 }
