115b9795472af2cc5def4fe0497a110d2e34c8ed
Sahanshah <k.sahanshah@gmail.com>
2022-10-17 09:40:16 +0530

    Time: 4 ms (37.41%), Space: 43.6 MB (77.55%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int compress(char[] chars) {
+        int currentCount = 1;
+        int k = 0;
+        for (int i = 1; i < chars.length; i ++) {
+            if (chars[i - 1] != chars[i]) {
+                chars[k++] = chars[i - 1];
+                if (currentCount > 1) {
+                    //chars[k++] = (char) (currentCount + '0');
+                    k += setDigit(chars, k, currentCount);
+                }
+                currentCount = 0;
+            }
+            currentCount ++;
+        }
+        
+        chars[k++] = chars[chars.length - 1];
+        if (currentCount > 1) {
+            //chars[k++] = (char) (currentCount + '0');
+            k += setDigit(chars, k, currentCount);
+        }
+        return k;
+    }
+    
+    private int setDigit(char[] chars, int k, int num) {
+        String t = String.valueOf(num);
+        for (int i = 0; i < t.length(); i ++, k ++) {
+            chars[k] = t.charAt(i);
+        }
+        return t.length();
+    }
+}
