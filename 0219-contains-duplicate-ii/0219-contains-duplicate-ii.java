03cae392f8d46e96f1a4321dce723705ec09cf9e
Sahanshah <k.sahanshah@gmail.com>
2022-10-21 09:49:16 +0530

    Time: 493 ms (5.71%), Space: 55 MB (84.80%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,17 +1,28 @@
 class Solution {
+    // public boolean containsNearbyDuplicate(int[] nums, int k) {
+    //     HashMap<Integer, Integer> map = new HashMap<>();
+    //     for (int i = 0; i < nums.length; i ++) {
+    //         if (map.containsKey(nums[i])) {
+    //             int diff = Math.abs(map.get(nums[i]) - i);
+    //             if (diff <= k) {
+    //                 return true;
+    //             } else if (diff > k) {
+    //                 map.put(nums[i], i);
+    //             }
+    //         } else {
+    //             map.put(nums[i], i);
+    //         }
+    //     }
+    //     return false;
+    // }
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i ++) {
-            if (map.containsKey(nums[i])) {
-                int diff = Math.abs(map.get(nums[i]) - i);
-                if (diff <= k) {
-                    return true;
-                } else if (diff > k) {
-                    map.put(nums[i], i);
-                }
-            } else {
-                map.put(nums[i], i);
+            Integer index = map.put(nums[i], i);
+            if (index != null && Math.abs(index - i) <= k) {
+                return true;
             }
+            System.out.println(index);
         }
         return false;
     }
