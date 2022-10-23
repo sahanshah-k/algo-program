531ec12bb02625d30714b31ff778f6c30424fe53
Sahanshah <k.sahanshah@gmail.com>
2022-10-23 17:09:48 +0530

    Time: 16 ms (26.77%), Space: 54.3 MB (72.93%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,22 +1,41 @@
 class Solution {
-    public int[] findErrorNums(int[] nums) {
-        int[] ary = new int[nums.length + 1];
-        int[] out = new int[2];
-        for (int i = 0; i < nums.length; i ++) {
-            if (ary[nums[i]] != 0) {
-                out[0] = nums[i];
-                //break;
-            }
-            ary[nums[i]] = nums[i];
-        }
+//     public int[] findErrorNums(int[] nums) {
+//         int[] ary = new int[nums.length + 1];
+//         int[] out = new int[2];
+//         for (int i = 0; i < nums.length; i ++) {
+//             if (ary[nums[i]] != 0) {
+//                 out[0] = nums[i];
+//                 //break;
+//             }
+//             ary[nums[i]] = nums[i];
+//         }
                 
-        for (int i = 1; i < nums.length + 1; i ++) {
-            if (ary[i] == 0) {
-                out[1] = i;
+//         for (int i = 1; i < nums.length + 1; i ++) {
+//             if (ary[i] == 0) {
+//                 out[1] = i;
+//                 break;
+//             }
+//         }
+        
+//         return out;
+//     }
+    public int[] findErrorNums(int[] nums) {
+        int[] out = new int[2];
+        
+        for (int n: nums) {
+            int abs = Math.abs(n);
+            if (nums[abs - 1] < 0) {
+                out[0] = abs;
+            }   
+            nums[abs - 1] = -Math.abs(nums[abs - 1]);
+        }
+        
+        for (int i = 0; i < nums.length; i ++) {
+            if (nums[i] > 0) {
+                out[1] = i + 1;
                 break;
             }
         }
-        
         return out;
     }
 }
