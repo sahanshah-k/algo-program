3041181c8e81a448867a9327979ca503d692d7d0
Sahanshah <k.sahanshah@gmail.com>
2022-11-07 10:47:43 +0530

    Time: 1 ms (88.30%), Space: 41.2 MB (32.93%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,12 +1,29 @@
 class Solution {
+    // public int maximum69Number (int num) {
+    //     char[] ca = String.valueOf(num).toCharArray();
+    //     for (int i = 0; i < ca.length; i ++) {
+    //         if (ca[i] == '6') {
+    //             ca[i] = '9';
+    //             break;
+    //         }
+    //     }
+    //     return Integer.parseInt(new String(ca));
+    // }
+    
+    
     public int maximum69Number (int num) {
-        char[] ca = String.valueOf(num).toCharArray();
-        for (int i = 0; i < ca.length; i ++) {
-            if (ca[i] == '6') {
-                ca[i] = '9';
-                break;
+        int temp = num;
+        int index = -1;
+        int i = 0;
+        while (temp > 0) {
+            int r = temp % 10;
+            if (r == 6) {
+                index = i;
             }
+            temp = temp / 10;
+            i ++;
         }
-        return Integer.parseInt(new String(ca));
+        return index != -1 ? (int) (num + 3 * Math.pow(10, index)) : num;
     }
+    
 }
